eed2a2e1f8258433ad8d68901012b46d65ab6a37
RockyKiran <rockykiran8085@gmail.com>
2024-11-06 13:09:37 +0000

    variables let,var,const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+let name="rocky"
+const rollNo=123
+var add="Odisha"
+
+/* 
+prefer not to use var
+because of issue in block scope and functional scope 
+*/
+
+console.log(name);
+console.table([name,rollNo,add]);
+
+console.log("ro");

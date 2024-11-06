8d37ec562d3ab1dc90627071cfbfda6ce91dab06
RockyKiran <rockykiran8085@gmail.com>
2024-11-06 13:08:37 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version 
+
+console.log("rocky");
+
+let name="rocky" // String 
+let age=12       // number
+let isLoggedIn=false  // Boolean
+
+// there are some premitive datatype
+/*
+number => 2 to power 53
+bigint => if we want to store the huge amount of number value 
+boolean => true/false
+null => stand alone value 
+undefined => not initilize the value
+ symbol => unique
+*/
+
+console.log(typeof "rocky"); // String 
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
+
+
+

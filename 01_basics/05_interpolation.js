bda4586e8f3bec6b6f413a19009f2b99f9ee248a
RockyKiran <rockykiran8085@gmail.com>
2024-11-07 06:38:45 +0000

    Interpolation,prompt,confirm

---
+++ 01_basics/05_interpolation.js
@@ -0,0 +1,13 @@
+let v1= Number(prompt("give me a number")) // here the user will give one value
+                                           // if sometimes the user giving something instead of number ,thn it will convert to number type (because here we using number constructor)
+    confirm(`you have give ${v1}`)         // here we will get one pop-up notification                              
+
+let v2= Number(prompt("give me a number"))
+ confirm(`you have give ${v2}`) 
+
+ console.log(v1)
+ console.log(v2)
+
+ document.write(`the summation of given number is ${v1+v2}`)  // here it will print in the UI/ webpage
+
+ 

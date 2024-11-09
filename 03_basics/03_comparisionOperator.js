ddea3ebca39a5625e1ebf48d93c988de3cfcdc71
RockyKiran <rockykiran8085@gmail.com>
2024-11-09 09:26:02 +0000

    Comparision Operator

---
+++ 03_basics/03_comparisionOperator.js
@@ -0,0 +1,82 @@
+// var x = 10
+// console.log(x)
+// //1)+=
+// x+=6
+// console.log(x)
+// //2)-=
+// x-=6
+// console.log(x)
+// //3)*=
+// x*=6
+// console.log(x)
+// //4)/=
+// x/=5
+// console.log(x)
+// //5)%=
+// x%=2
+// console.log(x)
+// //6)**=
+// x**=10
+// console.log(x)
+
+
+//comparision operator:-
+
+//1)==(data type not effected)
+console.log(10==10)
+console.log(10==20)
+console.log(10=='10')
+console.log(10=='20')
+console.log('dinga'=='dinga')
+console.log('dinga'=='dingi')
+console.log('dinga'==['dinga'])
+console.log('')
+//2)===(data type effected)
+console.log(10===10)
+console.log(10===20)
+console.log(10==='10')
+console.log(10==='20')
+console.log('dinga'==='dinga')
+console.log('dinga'==='dingi')
+// console.log('dinga'===['dinga'])
+console.log('')
+//3)!=(data type not effected)
+console.log(10!=10)
+console.log(10!=20)
+console.log(10!='10')
+console.log(10!='30')
+console.log('10'!=30)
+console.log('')
+//4)!==(data type effected)
+console.log(10!==10)
+console.log(10!==20)
+console.log(10!=='10')
+console.log(10!=='30')
+console.log('10'!==30)
+console.log('')
+//5)>(data type not effected)
+console.log(10>20)
+console.log(10>5)
+console.log(10>'20')
+console.log(10>'5')
+console.log('')
+//6)<(data type not effected)
+console.log(10<20)
+console.log(10<5)
+console.log(10<'20')
+console.log(10<'5')
+console.log('')
+//7)>=(data type not effected)
+console.log(10>=20)
+console.log(10>=5)
+console.log(10>='20')
+console.log(10>='5')
+console.log('')
+//8)(data type not effected)
+console.log(10<=20)
+console.log(10<=5)
+console.log(10<='20')
+console.log(10<='5')
+console.log('')
+
+

3b61050a96f4f8f5e2aba719a28076645a4186b7
RockyKiran <rockykiran8085@gmail.com>
2024-11-09 09:45:49 +0000

    Logical & Ternary Operator

--- 03_basics/02_logicalOperator.js
+++ 03_basics/02_logicalOperator.js
@@ -30,4 +30,22 @@ if(pass==dbPassword || conPass==dbPassword)
         console.error('you have entered inncorrect password')
     }
 
+// we have one another operator 
+
+//Nullish Coalescing Operator(??) : based upon null or undefined
+
+let value ;
+value=null ?? 10
+console.log(value);
+
+value=undefined ?? 15 ?? 20     // here initilize happen alwaysin first position
+console.log(value); 
+
+
+// Ternary Operator
+
+// condition ? true : false  
+
+let num =100 
+num<50 ? console.log("less than 50") : console.log("more than 50");
 
